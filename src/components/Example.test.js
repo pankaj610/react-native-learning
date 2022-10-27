61ad673f2ebd7872d2169a25a3f7fd1ed3686605
Pankaj Verma <pankajverma@sharechat.co>
2022-10-27 23:18:06 +0530

    React infinite query

--- src/components/Example.test.js
+++ src/components/Example.test.js
@@ -6,24 +6,24 @@ afterAll(()=> {
 
 },);
 
+const onEndReached = jest.fn();
+
 test('examples of some things', async () => {
     const expectedUsername = 'Ada Lovelace'
 
     render(<Example prop="1"/>)
 
     fireEvent.changeText(screen.getByTestId("input"), "Pankaj");
+    
     fireEvent.press(screen.getByText("Click here!"))
+    
     const userOutput = await screen.findByTestId('output');
-    expect(userOutput)
 
-    // fireEvent.changeText(screen.getByTestId('input'), expectedUsername)
-    // fireEvent.press(screen.getByText('Print Username'))
+    fireEvent.scroll(screen.getByTestId("flat-list"))
 
-    // // Using `findBy` query to wait for asynchronous operation to finish
-    // const usernameOutput = await screen.findByTestId('printed-username')
+    expect(onEndReached).toHaveBeenCalled();
 
-    // // Using `toHaveTextContent` matcher from `@testing-library/jest-native` package.
-    // expect(usernameOutput).toHaveTextContent(expectedUsername)
 
-    // expect(screen.toJSON()).toMatchSnapshot()
+    expect(userOutput).toHaveTextContent("Pankaj");
+    
 })
--- src/utils.js/utils.js
+++ src/utils.js/utils.js
@@ -1,5 +1,5 @@
 
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { TOKEN } from '../constants';
 export const actionCreator = (type, payload, ...rest) => {
     return ({
